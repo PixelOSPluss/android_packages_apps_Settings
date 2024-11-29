36017aa93cafde6157ac89d06388c9709da6ba15
Dhina17 <dhinalogu@gmail.com>
2024-11-29 11:14:27 +0000

    Settings: Move applock to apps section
    Change-Id: Ibc93eaa381e8c6410e44210d9cd2707723a6de20

--- src/com/android/settings/security/applock/AppLockPackageListFragment.kt
+++ src/com/android/settings/security/applock/AppLockPackageListFragment.kt
@@ -28,7 +28,7 @@ import androidx.lifecycle.lifecycleScope
 import androidx.preference.Preference
 
 import com.android.internal.logging.nano.MetricsProto
-import com.android.internal.util.clover.CloverUtils
+import com.android.internal.util.custom.CustomUtils
 
 import com.android.settings.R
 import com.android.settings.core.SubSettingLauncher
@@ -54,7 +54,7 @@ class AppLockPackageListFragment : DashboardFragment() {
         super.onAttach(context)
         appLockManager = context.getSystemService(AppLockManager::class.java)!!
         pm = context.packageManager
-        launchablePackages = CloverUtils.launchablePackages(context)
+        launchablePackages = CustomUtils.launchablePackages(context)
         whiteListedPackages = resources.getStringArray(
             com.android.internal.R.array.config_appLockAllowedSystemApps)
     }
