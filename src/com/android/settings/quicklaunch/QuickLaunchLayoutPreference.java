d76e9321b1d06746b4f787f76669de16426aa9ed
zenzer0s <praveenzalaki200@gmail.com>
2026-08-11 03:08:22 +0000

    Settings: Simplify QuickLaunchLayoutPreference layout and remove viewpager tutorial
    Change-Id: If1b896066c0028582ab299800c19084a501271cb

--- src/com/android/settings/quicklaunch/QuickLaunchLayoutPreference.java
+++ src/com/android/settings/quicklaunch/QuickLaunchLayoutPreference.java
@@ -9,7 +9,6 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.view.View;
 import android.widget.ImageView;
-import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.content.Intent;
 import android.content.pm.PackageManager;
@@ -19,8 +18,6 @@ import android.provider.Settings;
 import androidx.annotation.NonNull;
 import androidx.preference.Preference;
 import androidx.preference.PreferenceViewHolder;
-import androidx.recyclerview.widget.RecyclerView;
-import androidx.viewpager2.widget.ViewPager2;
 
 import com.android.settings.R;
 import com.google.android.material.materialswitch.MaterialSwitch;
@@ -29,6 +26,7 @@ public class QuickLaunchLayoutPreference extends Preference {
 
     public QuickLaunchLayoutPreference(Context context, AttributeSet attrs) {
         super(context, attrs);
+        setLayoutResource(R.layout.quick_launch_settings);
     }
 
     @Override
@@ -42,8 +40,6 @@ public class QuickLaunchLayoutPreference extends Preference {
         View view = holder.itemView;
         Context context = getContext();
 
-        ViewPager2 viewPager = view.findViewById(R.id.tutorial_viewpager);
-        LinearLayout indicatorContainer = view.findViewById(R.id.indicator_container);
         MaterialSwitch mainSwitch = view.findViewById(R.id.quick_launch_switch);
         TextView btnEdit = view.findViewById(R.id.btn_edit_menu);
 
@@ -51,11 +47,6 @@ public class QuickLaunchLayoutPreference extends Preference {
         ImageView slotLeft = view.findViewById(R.id.slot_left_icon);
         ImageView slotCenter = view.findViewById(R.id.slot_center_icon);
         ImageView slotRight = view.findViewById(R.id.slot_right_icon);
-
-        if (viewPager != null && viewPager.getAdapter() == null) {
-            viewPager.setAdapter(new QuickLaunchSettings.TutorialAdapter(context));
-            setupIndicators(context, indicatorContainer, viewPager);
-        }
 
         if (mainSwitch != null) {
             boolean enabled = Settings.Secure.getInt(context.getContentResolver(),
@@ -75,32 +66,6 @@ public class QuickLaunchLayoutPreference extends Preference {
         }
 
         loadSlotIcons(context, slotTop, slotLeft, slotCenter, slotRight);
-    }
-
-    private void setupIndicators(Context context, LinearLayout container, ViewPager2 viewPager) {
-        if (container == null) return;
-        container.removeAllViews();
-        int count = 2;
-        ImageView[] dots = new ImageView[count];
-
-        for (int i = 0; i < count; i++) {
-            dots[i] = new ImageView(context);
-            LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(16, 16);
-            params.setMargins(8, 0, 8, 0);
-            dots[i].setLayoutParams(params);
-            dots[i].setImageResource(R.drawable.ic_settings_accent);
-            dots[i].setAlpha(i == 0 ? 1.0f : 0.3f);
-            container.addView(dots[i]);
-        }
-
-        viewPager.registerOnPageChangeCallback(new ViewPager2.OnPageChangeCallback() {
-            @Override
-            public void onPageSelected(int position) {
-                for (int i = 0; i < count; i++) {
-                    dots[i].setAlpha(i == position ? 1.0f : 0.3f);
-                }
-            }
-        });
     }
 
     private void loadSlotIcons(Context context, ImageView top, ImageView left,
